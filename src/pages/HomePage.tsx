61516db0032b7c6cacb4fa0c1d51d6696f77a80f
Alevar <alevar03@mail.ru>
2023-10-17 06:03:00 +0300

    Loading skeletons on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,20 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { Skeleton, VStack } from "@chakra-ui/react";
 import RouteCard from "../components/RouteCard";
 import useTrainRoutes from "../hooks/useTrainRoutes";
 
 export default () => {
-  const { data, error, isLoading } = useTrainRoutes();
+  const { data, isLoading } = useTrainRoutes();
+  const skeletons = [0, 1, 2];
   return (
     <VStack mt="46px" p={8} spacing={4}>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <Skeleton
+            w="100%"
+            h={{ base: "350px", lg: "120px" }}
+            key={skeleton}
+          />
+        ))}
       {data?.results.map((route) => (
         <RouteCard route={route} key={route.trainName} />
       ))}
